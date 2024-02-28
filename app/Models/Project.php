591b1a9804353be1657d9e644c8ffc59c9f6f604
pietrostraneo <straneo_p@yahoo.it>
2024-02-28 16:38:40 +0100

    Defined relationship on Project/Type models

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -9,5 +9,10 @@ class Project extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name', 'preview', 'description', 'supervisors', 'framework', 'assignment_date'];
+    protected $fillable = ['name', 'preview', 'description', 'type_id', 'supervisors', 'framework', 'assignment_date'];
+
+    public function type()
+    {
+        return $this->belongsTo(Type::class);
+    }
 }
